ed2a87a8c1cbcd44fb2d9f5f308153ea3769f44e
Raivo <raivozelcs@gmail.com>
2022-03-16 21:15:01 +0000

    My first JavaScript page

---
+++ script.js
@@ -0,0 +1,12 @@
+function userInput() {	
+ var email = document.getElementById("email").value;
+  var fname = document.getElementById("fname").value;
+		document.getElementById("message").value;
+		document.getElementById("message").innerHTML+=fname +", thank you for your details. We will be in touch via "+email+ " shortly.";
+		hide();
+}
+function hide(){
+	document.getElementById("container").style.display = "none";
+	
+	
+}
