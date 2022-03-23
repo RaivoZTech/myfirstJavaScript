768425064460ef0ff2d84c7fcf5e90e7ddd7c77d
Raivo <raivozelcs@gmail.com>
2022-03-23 18:51:46 +0000

    small changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function userInput() {
  var email = document.getElementById("email").value;
   var fname = document.getElementById("fname").value;
 		document.getElementById("message").value;
-		document.getElementById("message").innerHTML+=fname +", thank you for your details. We will be in touch via "+email+ " shortly.";
+		document.getElementById("message").innerHTML=fname +", thank you for your details. We will be in touch via "+email+ " shortly.";
 		hide();
 }
 function hide(){
